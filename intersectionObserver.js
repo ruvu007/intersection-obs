9a6550402730de44cdbed1b9f276660aa792616a
Ruben V <ralvuurman@gmail.com>
2021-01-13 12:22:47 +0100

    Video 2

--- intersectionObserver.js
+++ intersectionObserver.js
@@ -1,4 +1,17 @@
 let Links = document.querySelectorAll('nav a');
+let secties = document.querySelectorAll('section');
+
+const opties = {};
+
+const verwerkDoorsnijden = (entries, observer) => {
+    entries.forEach( entry => {
+        console.log(entry.target + "doorsnijdt" + entry.isIntersecting);
+    })
+}
+
+let observer = new IntersectionObserver(verwerkDoorsnijden, opties);
+
+observer.observe(secties[1]);
 
 const verwijderActief = () => {
     Links.forEach( (link) => {
